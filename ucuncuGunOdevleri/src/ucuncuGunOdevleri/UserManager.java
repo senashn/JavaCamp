6c7267c86036293dcfbb23efff12a5faf4a9bbba
senashn <senashn99@outlook.com>
2021-05-06 16:44:01 +0300

    ucucuGunOdevleri
    ucucuGunOdevleri

---
+++ ucuncuGunOdevleri/src/ucuncuGunOdevleri/UserManager.java
@@ -0,0 +1,11 @@
+package ucuncuGunOdevleri;
+
+public class UserManager {
+	public void login(User user) {
+		System.out.println(user.getName() + " Giriþ yaptý");
+	}
+	
+	public void logout(User user) {
+		System.out.println(user.getName() + " Çýkýþ yaptý");
+	}
+}
